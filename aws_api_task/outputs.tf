c257461b84878d5bbb44c8906d3d91402d912af8
onyankupon <olapadew30@gmail.com>
2023-06-20 14:00:22 +0200

    added description

--- aws_api_task/outputs.tf
+++ aws_api_task/outputs.tf
@@ -1,17 +1,20 @@
 // Print the  exact path of GET and POST,
 output "get_url" {
+  description = "URL for get api"
   value = "${aws_api_gateway_stage.api_stage.invoke_url}/{user_id}"
 }
 
 output "post_url" {
+  description = "URL for post api"
   value = "${aws_api_gateway_stage.api_stage.invoke_url}/users"
-
 }
 
 output "delete_url" {
+  description = "URL for delete api"
   value = "${aws_api_gateway_stage.api_stage.invoke_url}/{user_id}"
 }
 
 output "scan_url" {
+  description = "URL for scan api"
   value = "${aws_api_gateway_stage.api_stage.invoke_url}/allusers"
 }
